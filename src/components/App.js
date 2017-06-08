a5f62dab9105ad9b0c9d68126e24d1597641f48e
github username <github email address>
2017-06-08 12:08:57 -0400

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,11 @@ class App extends React.Component {
 
   constructor() {
     super();
-    
+
     this.state = {
       latestTweets: []
     };
+
   }
 
   // TODO: componentWillMount()
@@ -28,6 +29,18 @@ class App extends React.Component {
     this.setState({
       latestTweets: newTweets
     });
+  }
+
+  componentWillMount(){
+    this.fetchTweets()
+  }
+
+  componentDidMount(){
+    this.startInterval()
+  }
+
+  componentWillUnmount(){
+    this.cleanUpInterval()
   }
 
   render() {
--- src/components/TweetWall.js
+++ src/components/TweetWall.js
@@ -4,7 +4,7 @@ import Tweet from './Tweet';
 class TweetWall extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       tweets: []
     };
@@ -13,6 +13,19 @@ class TweetWall extends React.Component {
   // TODO: componentWillMount()
   // TODO: shouldComponentUpdate()
   // TODO: componentWillReceiveProps()
+
+  componentWillMount(){
+    this.setState({ tweets: this.props.newTweets })
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({tweets: [...nextProps.newTweets, ...this.state.tweets]})
+  }
+
+  shouldComponentUpdate(){
+    
+  }
+
 
   render() {
     const tweets = this.state.tweets.map((tweet, index) => <Tweet text={tweet.text} key={index} />);
